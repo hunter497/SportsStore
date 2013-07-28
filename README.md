51713569fe0716e4f0f455099c468e619d044915
Nick Hunter <hunter.497@gmail.com>
2013-07-28 18:35:18 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+SportsStore
+===========
+
+A more thorough tutorial by Adam Freeman, featured in his book "Pro ASP.NET MVC 4"
---
+++ SportsStore.WebUI/Models/SelectedCategoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿// WIP
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Models
+{
+    public class SelectedCategoryViewModel
+    {
+        public IEnumerable<string> Categories { get; set; }
+        public string SelectedCategory { get; set; }
+    }
+}
